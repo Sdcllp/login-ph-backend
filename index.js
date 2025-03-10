460ac571af0843cc82e129efa1a32cc5f1403c38
Namu2024 <namrata.birhade@partner.designcafe.com>
2025-03-10 16:27:09 +0530

    changes in index

--- index.js
+++ index.js
@@ -18,5 +18,13 @@ mongoose
 
 app.use("/api/auth", authRoutes);
 
+app.get('/', (req, res) => {
+  res.json({
+    activeStatus: true,
+    error: false,
+    message: `Backend is running on ${process.env.LOCAL_IP}:${process.env.PORT}`,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
